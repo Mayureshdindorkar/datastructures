fb91019b5f5676aebfe55041aa090ac16bf3a17a
Mayuresh Dindorkar <dmayuresh99@gmail.com>
2018-09-22 16:21:47 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"\n****************Doing Bubble Sort***************"<<endl;
+    int i,j,n;
+    float temp;
+
+
+    cout<<"\nEnter number of students in class :";
+	cin>>n;
+	float b[n];
+
+	cout<<"\nEnter SGPA of students:";
+	for(i=0;i<n;i++)
+	cin>>b[i];
+
+	cout<<"\nEntered SGPA of students are   :";
+	for(i=0;i<n;i++)
+	cout<<b[i]<<"\t";
+
+	for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(b[j]>b[j+1])
+            {
+                temp=b[j];
+                b[j]=b[j+1];
+                b[j+1]=temp;
+            }
+        }
+    }
+    cout<<"\nBubble sorted array :";
+	for(i=0;i<n;i++)
+	cout<<b[i]<<"\t";
+}
---
+++ insertionsort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   int i,j,n;
+    float temp;
+
+    cout<<"\n****************Doing insertion Sort***************"<<endl;
+
+	cout<<"\nEnter number of students in class :";
+	cin>>n;
+	float a[n];
+
+	cout<<"\nEnter SGPA of students :";
+	for(i=0;i<n;i++)
+	cin>>a[i];
+
+	cout<<"\nEntered SGPA of students are  :";
+	for(i=0;i<n;i++)
+	cout<<a[i]<<"\t";
+
+    for(i=1;i<n;i++)
+    {
+        temp=a[i];
+        j=i-1;
+
+        while(j>=0 && temp<a[j])
+        {
+            a[j+1]=a[j];
+            j--;
+        }
+        a[j+1]=temp;
+    }
+
+    cout<<"\nDisplaying elements of array sorted by selection/Sequential sort are :";
+	for(i=0;i<n;i++)
+	cout<<a[i]<<"\t";
+	cout<<"\n";
+
+}
